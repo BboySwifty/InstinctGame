f7553cb38e598c092d37cffec8a184fc63a6a42b
Alain Abboud <abboudalain@gmail.com>
2022-05-06 19:08:22 -0400

    ColorToggle

---
+++ Assets/Scripts/UI/ColorToggle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorToggle : MonoBehaviour
+{
+    public Color[] colors;
+    public Image colorImage;
+
+    private int index = 0;
+
+    void Start()
+    {
+        colorImage.color = colors[index];
+    }
+
+    public void NextColor()
+    {
+        index++;
+        if (index == colors.Length)
+            index = 0;
+        colorImage.color = colors[index];
+    }
+}
